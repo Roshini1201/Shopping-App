1342a0238791b3edbfe305f8bbb6c188f41a5cb9
Roshini1201 <krsvroshini1201@gmail.com>
2026-03-24 23:06:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// initialize counter
+let cartCount = 0;
+
+function addToCart() {
+    cartCount++; // increase count
+    alert("Item added to cart successfully!\nTotal items in cart: " + cartCount);
+}
